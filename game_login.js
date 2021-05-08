883645a42092842c6e3ba90c5c8cd17187dea929
Kheer-Thakkar <77600784+Kheer-Thakkar@users.noreply.github.com>
2021-05-08 20:07:38 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,8 @@
+function adduser(){
+    player1_name=document.getElementById("y").value;
+    player2_name=document.getElementById("b").value;
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+    window.location.replace("game_page.html");
+
+}
